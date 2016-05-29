2225de848467ffe57ac434c205ad46c5d67e0acf
nsmalimov <norman000@mail.ru>
2016-05-29 19:03:40 +0400

    Update Constants.java

--- app/src/main/java/utils/Constants.java
+++ app/src/main/java/utils/Constants.java
@@ -3,5 +3,5 @@ package utils;
 public class Constants {
     public static String apiWeatherUrl = "http://api.openweathermap.org/";
 
-    public static String apiKey = "7eea3ebb608c42f91e500a321ed876b6";
+    public static String apiKey = "";
 }
